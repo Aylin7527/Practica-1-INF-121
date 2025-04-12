3c41a2064a4fc338fd4753c87f62ce86386a28eb
Aylin Michel <149416390+Aylin7527@users.noreply.github.com>
2025-04-11 22:07:04 -0400

    Ejercicio3.py

--- INTRODUCCIONPOO/EJERCICIO3/Ejercicio3.py
+++ INTRODUCCIONPOO/EJERCICIO3/Ejercicio3.py
@@ -14,7 +14,7 @@ class Coche:
             self.__velocidad = 0
         print(f"{self.__marca} {self.__modelo} frena. Nueva velocidad: {self.__velocidad} km/h")
 
-    def mostrar_velocidad(self):
+    def mostrarVelocidad(self):
         print(f"El coche {self.__marca} {self.__modelo} va a {self.__velocidad} km/h")
 
 coche1 = Coche("Toyota", "Corolla", 50)
@@ -22,8 +22,8 @@ coche2 = Coche("Ford", "Focus", 30)
 
 coche1.acelerar()
 coche1.frenar()
-coche1.mostrar_velocidad()
+coche1.mostrarVelocidad()
 
 coche2.acelerar()
 coche2.frenar()
-coche2.mostrar_velocidad()
+coche2.mostrarVelocidad()
